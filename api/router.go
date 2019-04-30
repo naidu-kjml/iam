4e99203106676197a8c41a851b9bb2038bb3ec5b
Martin Bajanik <martin.bajanik@kiwi.com>
2019-04-30 14:24:35 +0200

    jwt: Add portalo public key

--- api/router.go
+++ api/router.go
@@ -1,14 +1,27 @@
 package api
 
 import (
+	"net/http"
+	"strings"
+
 	"gitlab.skypicker.com/platform/security/iam/security"
 	"gitlab.skypicker.com/platform/security/iam/services/okta"
 	"gopkg.in/DataDog/dd-trace-go.v1/contrib/julienschmidt/httprouter"
 )
 
+const wellKnownFolder string = ".well-known"
+
 // CreateRouter creates a new router instance
 func CreateRouter(serviceName string, oktaClient *okta.Client, secretManager security.SecretManager) *httprouter.Router {
 	router := httprouter.New(httprouter.WithServiceName(serviceName))
+
+	router.Handler(
+		"GET",
+		"/"+wellKnownFolder+"/*filepath",
+		DisableDirectoryListingHandler(
+			http.StripPrefix("/"+wellKnownFolder+"/", http.FileServer(http.Dir(wellKnownFolder))),
+		),
+	)
 
 	// Healthcheck routes.
 	router.GET("/healthcheck", healthcheck)
@@ -43,4 +56,15 @@ func CreateRouter(serviceName string, oktaClient *okta.Client, secretManager sec
 	router.PanicHandler = panicHandler
 
 	return router
+}
+
+// DisableDirectoryListingHandler prevents directory listings to be returned to the user-agent.
+func DisableDirectoryListingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
