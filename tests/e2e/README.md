99020a6207e4c3acc8fb207d28c9c2ae51e32770
Angelo Gazzola <angelo@nglgzz.com>
2019-05-14 17:11:38 +0200

    Tests: refactor e2e docker-compose and makefile

--- tests/e2e/README.md
+++ tests/e2e/README.md
@@ -9,6 +9,11 @@ When running locally it is important to first set up environment variables in yo
 ```sh
 export GITLAB_USERNAME=tester@kiwi.com
 export GITLAB_PASSWORD=your-access-token-from-gitlab
-make build-iam
-make run-e2e
+make e2e
 ```
+
+If you didn't make changes on IAM or mocks (eg. you only created e2e tests) you
+can run `make e2e/nobuild` which will skip the image builds.
+
+Both `make e2e` and `make e2e/nobuild` start a test environment using
+docker-compose, to tear down this environment run `e2e/env-stop`.
--- tests/e2e/e2e.mk
+++ tests/e2e/e2e.mk
@@ -1,41 +1,29 @@
 # This file should be included into main Makefile !
 ROOT_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
-run-e2e:
-	make test-e2e-env-start
-	make test-e2e-iam
-	make test-e2e-venom
+e2e:
+	$(call log_info,Starting test environment:)
+	cd $(ROOT_DIR) && docker-compose up -d --build nginx redis kiwi-iam
+	$(call log_info,Test environment started!)
+	make e2e/venom
 
+e2e/nobuild: ## Run tests without rebuilding images
+	$(call log_info,Starting test environment:)
+	cd $(ROOT_DIR) && docker-compose up -d nginx redis kiwi-iam
+	$(call log_info,Test environment started!)
+	make e2e/venom
 
-# test-e2e-env-start starts the test environment with docker-compose:
-# 1. It starts nginx service
-# 2. It starts redis service
-test-e2e-env-start: ## Start test environment
-	$(call before_job,Starting test environment:)
-	cd $(ROOT_DIR) && docker-compose up -d --build nginx redis
-	$(call after_job,Test environment started!)
-
-# Builds the IAM container with required build arguments
-build-iam:
-	cd $(ROOT_DIR) && docker-compose build --build-arg GITLAB_USERNAME --build-arg GITLAB_PASSWORD kiwi-iam
-
-# test-e2e-iam simply just starts iam service
-test-e2e-iam: ## Start iam
-	$(call before_job,Starting kiwi-iam:)
-	cd $(ROOT_DIR) && docker-compose up -d kiwi-iam  
-	$(call after_job,kiwi-iam started!)
-
-test-e2e-venom:
-	$(call before_job,Starting venom tests:)
+e2e/venom:
+	$(call log_info,Starting venom tests:)
 	cd $(ROOT_DIR) && docker-compose build venom
 	$(eval VENOM_CONTAINER_NAME = venom-container-$(shell date +%s))
 	cd $(ROOT_DIR) && docker-compose run -d --name $(VENOM_CONTAINER_NAME) venom tail -f /dev/null
 	docker exec $(VENOM_CONTAINER_NAME) venom run --var-from-file variables.yml --parallel 5 --format=xml --output-dir="." --strict ; \
 	let "EXIT_CODE=$$?" ; \
 	cd $(ROOT_DIR) && docker exec  $(VENOM_CONTAINER_NAME) cat test_results.xml > test_results.xml ; \
-	docker-compose rm -f -v venom &1>/dev/null || true ; \
+	docker-compose rm --force --stop -v venom || true ; \
 	exit $$EXIT_CODE
-	$(call after_job,Venom integration tests completed!)
+	$(call log_success,Venom integration tests completed!)
 
-test-e2e-env-stop: ## Stop test environment
+e2e/env-stop: ## Stop test environment
 	cd $(ROOT_DIR) && docker-compose down -v
