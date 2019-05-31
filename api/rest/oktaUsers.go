9eae55a402c9f937dfa915c9c96d6f74b179f153
Martin Bajanik <martin.bajanik@kiwi.com>
2019-05-31 14:13:34 +0200

    e2e: Basic tests for permissions

--- api/rest/oktaUsers.go
+++ api/rest/oktaUsers.go
@@ -59,7 +59,7 @@ func getOktaUserByEmail(client userDataService) httprouter.Handle {
 			log.Println("[ERROR]", err.Error())
 			raven.CaptureError(err, nil)
 		}
-		oktaUser.OktaID = ""           // OktaID is important only internally, service have no use for it
+		oktaUser.OktaID = ""           // OktaID is used only internally
 		oktaUser.GroupMembership = nil // GroupMembership is used only internally
 
 		w.Header().Set("Content-Type", "application/json")
