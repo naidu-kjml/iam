bf3b11a3ccbc60af297dc39be89219a93c3ae9b1
Angelo Gazzola <angelo@nglgzz.com>
2019-06-18 16:26:16 +0200

    GitlabCI: improve e2e tests by pulling IAM image

--- tests/e2e/e2e.mk
+++ tests/e2e/e2e.mk
@@ -7,9 +7,11 @@ e2e:
 	$(call log_info,Test environment started!)
 	make e2e/venom
 
-e2e/nobuild: ## Run tests without rebuilding images
+e2e/nobuild: ## Run tests without rebuilding IAM
 	$(call log_info,Starting test environment:)
-	cd $(ROOT_DIR) && docker-compose up -d nginx redis kiwi-iam
+	cd $(ROOT_DIR) && \
+	docker-compose up -d --build nginx redis && \
+	docker-compose up -d --no-build kiwi-iam
 	$(call log_info,Test environment started!)
 	make e2e/venom
 
