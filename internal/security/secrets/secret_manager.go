b4d92ce8b01ffb4fa6ed1bbef1dc22db24fe4ced
ci-build <ci-build@kiwi.com>
2019-07-24 12:35:30 +0000

    Publishing to gh-pages (publish.sh)

--- internal/security/secrets/secret_manager.go
+++
@@ -1,7 +0,0 @@
-package secrets
-
-// SecretManager is a interface that describes how we want to use secrets
-type SecretManager interface {
-	GetAppToken(string, string) (string, error)
-	GetSetting(string) (string, error)
-}
