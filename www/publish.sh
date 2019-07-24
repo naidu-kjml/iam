e730d39716c71c5ade0f20027c61afd263a0ea7d
Angelo Gazzola <angelo@nglgzz.com>
2019-07-24 15:05:13 +0200

    CI: do not fail docs/publish if there are no changes
    And only push to gh-pages, not all branches.

--- www/publish.sh
+++ www/publish.sh
@@ -22,8 +22,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" || true
 
-#echo "Pushing to github"
-git push --all
-
+echo "Pushing to github"
+git push origin gh-pages
