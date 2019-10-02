65806686ccb0fe544f14edd01b9aac5ee0a533f5
Simon Prochazka <simon@fallion.net>
2019-10-02 17:23:15 +0200

    feat: port middleware_security
    - ports middleware_security from old rest package and updates the functions to use the new server struct

---
+++ api/rest/v1/middleware_security.go
@@ -0,0 +1,65 @@
+package rest
+
+import (
+	"log"
+	"net/http"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+
+	"github.com/kiwicom/iam/api"
+	"github.com/kiwicom/iam/internal/monitoring"
+	"github.com/kiwicom/iam/internal/security"
+)
+
+// AuthWrapper wraps a router to validate the authentication token
+func (s *server) middlewareSecurity(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := s.checkAuth(r)
+		if err != nil {
+			if apiErr, ok := err.(api.Error); ok {
+				http.Error(w, apiErr.Message, apiErr.Code)
+			} else {
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+			}
+
+			log.Println("[ERROR]", err.Error())
+			return
+		}
+
+		// Delegate request to the given handle
+		h(w, r)
+	}
+}
+
+// checkAuth checks if user has proper token + user agent
+func (s *server) checkAuth(r *http.Request) error {
+	requestToken, err := security.GetToken(r.Header.Get("Authorization"))
+	if err != nil {
+		return api.Error{Message: "Use the Bearer {token} authorization scheme", Code: http.StatusUnauthorized}
+	}
+	userAgent := r.Header.Get("User-Agent")
+
+	service, err := security.GetService(userAgent)
+	if err != nil {
+		return api.Error{Message: err.Error(), Code: http.StatusUnauthorized}
+	}
+
+	if span, ok := tracer.SpanFromContext(r.Context()); ok {
+		span.SetTag("user-agent", userAgent)
+		span.SetTag("service-name", service.Name)
+	}
+
+	tokenErr := security.VerifyToken(s.secretManager, service, requestToken)
+
+	if tokenErr != nil {
+		return api.Error{Message: "Unauthorized: " + tokenErr.Error(), Code: http.StatusUnauthorized}
+	}
+
+	s.metricClient.Incr(
+		"incoming.requests",
+		monitoring.Tag("service-name", service.Name),
+		monitoring.Tag("service-environment", service.Environment),
+	)
+
+	return nil
+}
--- api/rest/v1/server.go
+++ api/rest/v1/server.go
@@ -4,11 +4,20 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/kiwicom/iam/internal/security/secrets"
 )
+
+type metricService interface {
+	// Incr increments by 1 a metric identified by name.
+	// tags should be in format name:value and can be created with Tag function to escape the values
+	Incr(string, ...string)
+}
 
 // server houses all dependencies and routing of the server
 type server struct {
-	router *mux.Router
+	router        *mux.Router
+	secretManager secrets.SecretManager
+	metricClient  metricService
 }
 
 func newServer() server {
