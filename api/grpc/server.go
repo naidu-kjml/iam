a83b723f1fd9b1ec99dc53c5766dc42cfb7a0d99
Matous Dzivjak <matousdzivjak@gmail.com>
2020-01-13 16:15:16 +0100

    feat: Add permissions to grpc user
    Add permission field to grpc user
    endpoint so it can be used with
    Kiwi Py-IAM middleware.
    
    Signed-off-by: Matous Dzivjak <matousdzivjak@gmail.com>

--- api/grpc/server.go
+++ api/grpc/server.go
@@ -3,7 +3,11 @@ package grpc
 import (
 	"context"
 	"errors"
+	"log"
 	"strconv"
+
+	"github.com/getsentry/raven-go"
+	"google.golang.org/grpc/metadata"
 
 	pb "github.com/kiwicom/iam/api/grpc/v1"
 	"github.com/kiwicom/iam/internal/services/okta"
@@ -25,11 +29,29 @@ func CreateServer(userServiceClient userDataService) (*Server, error) {
 }
 
 // User returns a single user based on email
-func (s *Server) User(_ context.Context, in *pb.UserRequest) (*pb.UserResponse, error) {
+func (s *Server) User(ctx context.Context, in *pb.UserRequest) (*pb.UserResponse, error) {
 	user, userErr := s.userService.GetUser(in.Email)
 	if userErr != nil {
 		return nil, userErr
 	}
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errMissingMetadata
+	}
+
+	// service-agent is used as gRPC tools currently don't allow for overriding user-agent
+	if len(md[metadataUserAgent]) == 0 {
+		return nil, errBadUA
+	}
+
+	permErr := s.userService.AddPermissions(&user, md[metadataUserAgent][0])
+	if permErr != nil {
+		log.Println("[ERROR]", permErr.Error())
+		raven.CaptureError(permErr, nil)
+		return nil, errors.New("unexpected server error")
+	}
+
 	employeeNumber, intErr := strconv.ParseInt(user.EmployeeNumber, 10, 64)
 	if intErr != nil {
 		return nil, errors.New("unexpected server error")
@@ -59,6 +81,7 @@ func (s *Server) User(_ context.Context, in *pb.UserRequest) (*pb.UserResponse, 
 		Location:       user.Location,
 		Manager:        user.Manager,
 		TeamMembership: user.TeamMembership,
+		Permissions:    user.Permissions,
 		Boocsek:        &attributes,
 	}, nil
 }
