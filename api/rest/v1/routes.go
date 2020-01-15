821625eb5f3c514d6c8d7d12e47abd2e744d1f90
Simon Prochazka <simon@fallion.net>
2020-01-15 16:18:24 +0100

    fix(rest): return back well known folder

--- api/rest/v1/routes.go
+++ api/rest/v1/routes.go
@@ -1,14 +1,37 @@
 package rest
 
-import tracingRouter "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorilla/mux"
+import (
+	"net/http"
+	"strings"
+
+	tracingRouter "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorilla/mux"
+)
 
 // routes handles registering all routes. All routes should be added here.
 func (s *Server) routes() {
+	const wellKnownFolder string = ".well-known"
+
 	s.Router = tracingRouter.NewRouter(tracingRouter.WithServiceName(s.ServiceName))
+
 	s.Router.HandleFunc("/", s.handleHello())
 	s.Router.HandleFunc("/healthcheck", s.handleHealthcheck())
 	s.Router.HandleFunc("/v1/teams", s.middlewareSecurity(s.handleTeamsGET()))
 	s.Router.HandleFunc("/v1/user", s.middlewareSecurity(s.handleUserGET()))
 	s.Router.HandleFunc("/v1/permissions", s.middlewareSecurity(s.handlePermissionsGET()))
 	s.Router.HandleFunc("/v1/groups", s.middlewareSecurity(s.handleGroupsGET()))
+
+	s.Router.PathPrefix("/" + wellKnownFolder + "/").Handler(DisableDirectoryListingHandler(
+		http.StripPrefix("/"+wellKnownFolder+"/", http.FileServer(http.Dir(wellKnownFolder))),
+	))
+}
+
+// DisableDirectoryListingHandler prevents directory listings to be returned to the user-agent.
+func DisableDirectoryListingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
